3c0896dd4f3472e6c02b741f672ef07dd8f15afe
unknown <JShen@ncl0114770.fda.gov>
2013-05-02 21:03:05 -0500

    First commit

---
+++ src/gov/fda/dbtk/PubMedRef.java
@@ -0,0 +1,27 @@
+package gov.fda.dbtk;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: JShen
+ * Date: 5/2/13
+ * Time: 8:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PubMedRef {
+    static int pmid;
+    static String journal;
+    static String volumn;
+    static String issue;
+    static String fpage;
+    static String lpage;
+    static String year;
+    static String fauthor;
+    static String authors;
+    static String title;
+    static String aabs;
+    static String doi;
+
+
+
+
+}
