5ac3f03794c8350495bc91c9616a6711d513b8a1
JShen <jie.shen@fda.hhs.gov>
2013-05-02 22:08:47 -0500

    test2

--- src/gov/fda/dbtk/PubMedRef.java
+++ src/gov/fda/dbtk/PubMedRef.java
@@ -1,5 +1,13 @@
 package gov.fda.dbtk;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
+import java.net.URL;
+
 /**
  * Created with IntelliJ IDEA.
  * User: JShen
@@ -8,20 +16,38 @@ package gov.fda.dbtk;
  * To change this template use File | Settings | File Templates.
  */
 public class PubMedRef {
-    static int pmid;
-    static String journal;
-    static String volumn;
-    static String issue;
-    static String fpage;
-    static String lpage;
-    static String year;
-    static String fauthor;
-    static String authors;
-    static String title;
-    static String aabs;
-    static String doi;
+    int pmid;
+    String journal;
+    String volumn;
+    String issue;
+    String fpage;
+    String lpage;
+    String year;
+    String fauthor;
+    String[] authors;
+    String title;
+    String aabs;
+    String doi;
+
+    // Constructor initialize
+    public PubMedRef(String pmidString) throws Exception{
+        //get an remote XML and transform to Document
+        String urlString="http://www.ncbi.nlm.nih.gov/pubmed/"+pmidString+"?report=xml";
+        URL xmlUrl= new URL(urlString);
+        InputStream xml = xmlUrl.openStream();
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(xml);
+        xml.close();
+
+        Node root=doc.getDocumentElement();
+        parserNode(root);
+    }
+
+    private void parserNode(Node node){
 
 
+    }
 
 
 }
